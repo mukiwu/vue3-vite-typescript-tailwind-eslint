462b5101608eafd9f0b90f5c30128b8fdd1ed0ea
mukiwu <mukispace@gmail.com>
2022-11-12 00:05:35 +0800

    first commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,20 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+    es2021: true
+  },
+  extends: [
+    'eslint:recommended',
+    '@vue/typescript/recommended',
+    'plugin:vue/vue3-essential',
+    '@vue/standard'
+  ],
+  parserOptions: {
+    parser: '@typescript-eslint/parser',
+    ecmaVersion: 2020
+  },
+  rules: {
+    // 'semi': [2, 'never']
+  }
+}
